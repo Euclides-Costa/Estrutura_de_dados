ff687a05945625382d85a5fccdfadd0f502bcf3f
Euclides Costa <114588979+Euclides-Costa@users.noreply.github.com>
2023-05-04 18:40:42 -0300

    Update exe_005.c

--- Unidade-01/lista_01/exe_005.c
+++ Unidade-01/lista_01/exe_005.c
@@ -1,43 +1,43 @@
-// /*
-// 5. Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em
-// uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
+/*
+5. Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em
+uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
 
-// Entrada: A entrada contém um valor inteiro N.
-// Saída: Imprima o tempo lido no arquivo de entrada (segundos), convertido para
-// horas:minutos:segundos, conforme exemplo fornecido.
-// */
-// #include <stdio.h>
-// #include <math.h>
+Entrada: A entrada contém um valor inteiro N.
+Saída: Imprima o tempo lido no arquivo de entrada (segundos), convertido para
+horas:minutos:segundos, conforme exemplo fornecido.
+*/
+#include <stdio.h>
+#include <math.h>
 
-// int main(void)
-// {
-//   /*
-//     s  = segundo
-//     h = hora
-//     m = minuto
-//   */
+int main(void)
+{
+  /*
+    s  = segundo
+    h = hora
+    m = minuto
+  */
   
-//   int s = 0;
-//   int h = 0;
-//   int m = 0;
-//   float num = 0;
-//   float cont = 0;
-//   float soma = 0;
+  int s = 0;
+  int h = 0;
+  int m = 0;
+  float num = 0;
+  float cont = 0;
+  float soma = 0;
 
-//   printf("Digite um número real: ");
-//   scanf("%f", &num);
+  printf("Digite um número real: ");
+  scanf("%f", &num);
 
-//   h = num/3600;
-//   cont = num/3600;
-//   soma = cont - h;
+  h = num/3600;
+  cont = num/3600;
+  soma = cont - h;
 
-//   m = soma * 60;
-//   cont = soma * 60;
-//   soma = cont - m;
+  m = soma * 60;
+  cont = soma * 60;
+  soma = cont - m;
 
-//   s = ceil(soma * 60);
+  s = ceil(soma * 60);
 
-//   printf("%d:%d:%d", h, m, s);
+  printf("%d:%d:%d", h, m, s);
 
-//   return 0;
-// }
+  return 0;
+}
