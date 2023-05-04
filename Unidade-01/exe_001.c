f9a89485d7232084354ec250b1d7266b03d55a8e
Euclides Costa <114588979+Euclides-Costa@users.noreply.github.com>
2023-05-04 18:29:08 -0300

    Create exe_001.c

---
+++ Unidade-01/exe_001.c
@@ -0,0 +1,28 @@
+// #include <stdio.h>
+
+// int principal(void)
+// {
+//    /*
+//      s = sarário do vendedor
+//      t_v = total de vendas
+//      t_r = total a receber
+//    */
+//    nome do caractere[20];
+//    float s = 0, t_v = 0, t_r = 0;
+  
+//    printf("Digite o seu nome: ");
+//    scanf("%s", nome);
+//    printf("Digite o seu salário: ");
+//    scanf("%f", &s);
+//    printf("Digite o valor total das vendas: ");
+//    scanf("%f", &t_v);
+  
+//    printf("%s\n", nome);
+//    printf("%f\n", s);
+//    printf("%f\n", t_v);
+
+//    t_r = s + (t_v * 0,15);
+//    printf("TOTAL = R$ %.2f", t_r);
+  
+//    retorna 0;
+// }
